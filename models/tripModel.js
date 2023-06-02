60a90d51a7f8e27dc62df6d8d84f0150d185a9d4
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2023-06-02 15:42:08 +0300

    Configure env file, database connection, and create necessary models

---
+++ models/tripModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+'Departure': {type: Date, required: true},
+'Return': {type: Date, required: true},
+'Departure station id': {type: Number, required: true},
+'Departure station name': {type: String, required: true},
+'Return station id': {type: Number, required: true},
+'Return station name': {type: String, required: true},
+'Covered distance (m)': {type: Number, required: true},
+'Duration (sec)': {type: Number, required: true} })
+
+const Trip = mongoose.model('jorneys', tripSchema);
+
+module.exports = Trip;
